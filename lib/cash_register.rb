39dfb5fc1ce28d799518aedf0e3275cdf2c7de18
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-30 14:30:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class CashRegister
-  attr_accessor :total, :discount, :title, :price, :items
+  attr_accessor :total, :discount, :title, :price, :items, :last_transaction
   
   def initialize(discount=0)
     @total = 0.0
